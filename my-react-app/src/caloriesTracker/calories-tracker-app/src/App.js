1aac3dfb22ee4cd20b5f9c21e8c6c8e6322d0db8
Harin <harinkim@live.unc.edu>
2022-04-20 14:28:19 -0400

    design controls

--- my-react-app/src/caloriesTracker/calories-tracker-app/src/App.js
+++ my-react-app/src/caloriesTracker/calories-tracker-app/src/App.js
@@ -1,10 +1,23 @@
-
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import AppBar from './components/AppBar/AppBar';
+import AppControlsCounter from './components/AppControls/AppControlsCounter';
+import AppControlsDelete from './components/AppControls/AppControlsDelete';
+import AppControlsInputs from './components/AppControls/AppControlsInputs';
 
 const App = () => {
+  const[meals, setMeals] = useState([]);
+  const[mealName, setMealName] = useState("");
+  const[calories, setCalories] = useState(0);
+  const[openModal, setOpenModal] = useState(false);
+
+
   return (
     <div className="App">
-      <h1>Hell From React</h1>
+      <AppBar />
+      <AppControlsCounter />
+      <AppControlsDelete />
+      <AppControlsInputs />
     </div>
   );
 }
---
+++ my-react-app/src/caloriesTracker/calories-tracker-app/src/components/AppBar/AppBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AppBar = () => {
+  return (
+    <div className = "app__navbar">
+        <h1>
+            Calories Tracker <span>App</span>
+        </h1>
+    </div>
+  )
+}
+
+export default AppBar
