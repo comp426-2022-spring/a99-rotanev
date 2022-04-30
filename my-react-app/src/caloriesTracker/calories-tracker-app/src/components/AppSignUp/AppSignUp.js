3afef610ccd3b6140452f020195bd4f083eefff9
NeilVakharia45 <neildvakharia@gmail.com>
2022-04-30 14:22:17 -0400

    Added sign up

--- my-react-app/src/caloriesTracker/calories-tracker-app/src/components/AppSignUp/AppSignUp.js
+++ my-react-app/src/caloriesTracker/calories-tracker-app/src/components/AppSignUp/AppSignUp.js
@@ -4,9 +4,26 @@ const AppSignUp = () => {
   return (
     <div className = "app__signup">
         <h1>Sign Up</h1>
-        <form>
-        <label><b>Name</b></label>
-        <input type="text" placeholder="Enter Email" name="email" required />
+        <form>         
+            <label><b>First Name</b></label>
+            <input type="text" placeholder="Enter First Name" name="firstname" required />
+
+            <label><b>Last Name</b></label>
+            <input type="text" placeholder="Enter Last Name" name="lastname" required />
+
+            <label><b>Email</b></label>
+            <input type="email" placeholder="Enter Email" name="email" required />
+
+            <label><b>Birthday</b></label>
+            <input type="date" placeholder="Enter Birthday" name="birthday" required />
+
+            <label><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="password" required />
+
+            <label><b>Confirm Password</b></label>
+            <input type="password" placeholder="Enter Confirm Password" name="confirmpassword" required />
+
+            <input type="submit"  name="submitbuttonsignup" value="Sign Up!"/>
         </form>
     </div>
   )
