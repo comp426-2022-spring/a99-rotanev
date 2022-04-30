33176af444bb082201a80ae65537c4c4af36b18a
sauryaacharya <saurya.acharya9@gmail.com>
2022-04-30 15:13:53 -0400

    signup syling

--- my-react-app/src/caloriesTracker/calories-tracker-app/src/components/AppSignUp/AppSignUp.js
+++ my-react-app/src/caloriesTracker/calories-tracker-app/src/components/AppSignUp/AppSignUp.js
@@ -3,28 +3,44 @@ import React from 'react'
 const AppSignUp = () => {
   return (
     <div className = "app__signup">
-        <h1>Sign Up</h1>
-        <form>         
-            <label><b>First Name</b></label>
+        <h1 className = "signup">Sign Up</h1>
+        <div className = "formbox">
+        <form>    
+            <div className="one">    
+            <label><b>First Name: </b></label>
             <input type="text" placeholder="Enter First Name" name="firstname" required />
+            </div>
 
-            <label><b>Last Name</b></label>
+            <div className="one">
+            <label><b>Last Name:  </b></label>
             <input type="text" placeholder="Enter Last Name" name="lastname" required />
+            </div>
 
-            <label><b>Email</b></label>
+            <div className="one">
+            <label><b>Email:  </b></label>
             <input type="email" placeholder="Enter Email" name="email" required />
+            </div>
 
-            <label><b>Birthday</b></label>
+            <div className="one">
+            <label><b>Birthday:  </b></label>
             <input type="date" placeholder="Enter Birthday" name="birthday" required />
+            </div>
 
-            <label><b>Password</b></label>
+            <div className="one">
+            <label><b>Password: </b></label>
             <input type="password" placeholder="Enter Password" name="password" required />
+            </div>
 
-            <label><b>Confirm Password</b></label>
-            <input type="password" placeholder="Enter Confirm Password" name="confirmpassword" required />
+            <div className="one">
+            <label><b>Confirm Password: </b></label>
+            <input type="password" placeholder="Confirmed Password" name="confirmpassword" required />
+            </div>
 
+            <div className="one">
             <input type="submit"  name="submitbuttonsignup" value="Sign Up!"/>
+            </div>
         </form>
+      </div>
     </div>
   )
 }
