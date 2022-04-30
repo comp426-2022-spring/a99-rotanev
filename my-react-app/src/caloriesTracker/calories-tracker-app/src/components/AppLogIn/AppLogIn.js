ea92afc9c451f5213e767833282354923560d072
Harin <harinkim@live.unc.edu>
2022-04-30 16:17:23 -0400

    log in

--- my-react-app/src/caloriesTracker/calories-tracker-app/src/components/AppLogIn/AppLogIn.js
+++ my-react-app/src/caloriesTracker/calories-tracker-app/src/components/AppLogIn/AppLogIn.js
@@ -1,15 +1,32 @@
 import React from 'react'
 
-const AppLogIn = () => {
-    return (
-      <div className = "app__login">
-          <h1>Log In</h1>
-          <form>
-          <label><b>Name</b></label>
-          <input type="text" placeholder="Enter Name" name="name" required />
-          </form>
-      </div>
-    )
+const AppLogIn = ({logIn, email, password}) => {
+
+  const onLogInSubmitClick = ()=> {
+        logIn();
   }
-  
-  export default AppLogIn
+
+  return (
+    <div className = "app__login">
+        <h1 className = "login">Log In</h1>
+        <div className = "formbox">
+        <form>    
+            <div className="one">
+            <label><b>Email:  </b></label>
+            <input type="email" placeholder="Enter Email" name="email" value={email} required />
+            </div>
+
+            <div className="one">
+            <label><b>Password: </b></label>
+            <input type="password" placeholder="Enter Password" name="password" value={password} required />
+            </div>
+
+            <div className="one">
+            <input onClick={onLogInSubmitClick} type="submit" value="Log In!"/>
+            </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+export default AppLogIn;
